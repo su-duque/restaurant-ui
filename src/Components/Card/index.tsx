8c136ddc424f5ca07d7699dbe0be50beb5ab459f
Susana Duque <su.duquem@gmail.com>
2024-11-01 14:23:14 -0500

    Add style to the Card

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -48,7 +48,7 @@ const Card = (data: MealDetails) => {
       onClick={() => showProductDetails(data)}
     >
       <figure className='relative mb-2 w-full h-4/5'>
-        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>
+        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs capitalize m-2 px-3 py-0.5'>
           {category}
         </span>
         <img className='w-full h-full object-cover rounded-lg' src={imageURL} alt={name} />
