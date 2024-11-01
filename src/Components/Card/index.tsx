ab39a822a292f0a92ddf109aa2669747d6e892d4
Susana Duque <su.duquem@gmail.com>
2024-11-01 03:21:53 -0500

    Change the Icon of the card to a check if the product is already added to the cart

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,4 +1,4 @@
-import { Plus } from 'lucide-react';
+import { Plus, Check } from 'lucide-react';
 import { MealDetails } from '../../interfaces/meals.interfaces';
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context';
@@ -13,12 +13,33 @@ const Card = (data: MealDetails) => {
     context.closeCheckoutPanel();
   };
 
-  const addProductsToCart = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, productData: MealDetails) => {
+  const addProductsToCart = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    productData: MealDetails,
+  ) => {
     event.stopPropagation(); // avoid opening the Product Details panel when adding a product to the cart
     context.setCartProducts([...context.cartProducts, productData]);
     context.setCount(context.count + 1);
     context.openCheckoutPanel();
     context.closeProductDetails();
+  };
+
+  const renderIcon = (id: string) => {
+    const isProductInCart = context.cartProducts.filter((product) => product.id === id).length > 0;
+    return isProductInCart ? (
+      <div className='absolute top-0 right-0 flex justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1'>
+        <Check color='#ffffff' strokeWidth={3} />
+      </div>
+    ) : (
+      <button
+        className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
+        onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+          addProductsToCart(event, data);
+        }}
+      >
+        <Plus className='h-4 w-4' />
+      </button>
+    );
   };
 
   return (
@@ -28,18 +49,10 @@ const Card = (data: MealDetails) => {
     >
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>
-          {`${category} ${id}`}
-          {/* TODO: Delete ID de la card */}
+          {category}
         </span>
         <img className='w-full h-full object-cover rounded-lg' src={imageURL} alt={name} />
-        <button
-          className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-          onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-            addProductsToCart(event, data);
-          }}
-        >
-          <Plus className='h-4 w-4' />
-        </button>
+        {renderIcon(id)}
       </figure>
       <p className='flex justify-between items-center gap-2 px-2'>
         <span className='text-sm font-light'>{name}</span>
