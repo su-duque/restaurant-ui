7ee8f14d15823711585d2c972f9ea28314fd5200
Susana Duque <su.duquem@gmail.com>
2024-11-01 13:49:57 -0500

    Add styles to the OrdersCard component

--- src/Components/OrdersCard/index.tsx
+++ src/Components/OrdersCard/index.tsx
@@ -1,3 +1,5 @@
+import { CalendarFold, ChevronRight, ShoppingBasket } from 'lucide-react';
+
 interface OrdersCardProps {
   date: string;
   totalPrice: number;
@@ -6,12 +8,23 @@ interface OrdersCardProps {
 
 const OrdersCard = ({ totalPrice, totalProducts, date }: OrdersCardProps) => {
   return (
-    <div className='flex justify-between items-center mb-3 px-6 border border-black rounded-lg'>
-      <p>
-        <span>Date: {date}</span>
-        <span>Total Products: {totalProducts}</span>
-        <span>Price: {totalPrice}</span>
-      </p>
+    <div className='flex justify-between items-center mb-4 px-6 border border-black rounded-lg p-4 w-80'>
+      <div className='flex justify-between w-full'>
+        <p className='flex flex-col'>
+          <div className='flex gap-2 mb-1'>
+            <CalendarFold />
+            <span className='font-light'>{date}</span>
+          </div>
+          <div className='flex gap-2'>
+            <ShoppingBasket />
+            <span className='font-light'>{totalProducts} articles</span>
+          </div>
+        </p>
+        <p className='flex items-center gap-2'>
+          <span className='font-medium text-2xl'>${totalPrice}</span>
+          <ChevronRight />
+        </p>
+      </div>
     </div>
   );
 };
