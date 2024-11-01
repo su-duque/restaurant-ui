d5d896be59a4fcafe4fdf18f3ddfd403a4e3f1ee
Susana Duque <su.duquem@gmail.com>
2024-11-01 12:31:27 -0500

    Render OrdersCard in MyOrders page

--- src/Pages/MyOrders/index.tsx
+++ src/Pages/MyOrders/index.tsx
@@ -1,5 +1,27 @@
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context';
+import OrdersCard from '../../Components/OrdersCard';
+
 function MyOrders() {
-  return <>MyOrders</>;
+  const context = useContext(ShoppingCartContext);
+
+  return (
+    <>
+      <div className='flex w-80 items-center relative justify-center mb-6'>
+        <h1>My Orders</h1>
+      </div>
+      {context.order.map((order, index) => (
+        <Link key={index} to={`/my-orders/${index}`}>
+          <OrdersCard
+            totalPrice={order.totalPrice}
+            date={order.date}
+            totalProducts={order.totalProducts}
+          />
+        </Link>
+      ))}
+    </>
+  );
 }
 
 export default MyOrders;
