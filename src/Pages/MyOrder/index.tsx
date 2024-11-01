06703d4986a99bbe4542f5a8ee1b49cdd8c750ae
Susana Duque <su.duquem@gmail.com>
2024-11-01 12:33:07 -0500

    Add functionality on MyOrder page to redirect to MyOrders (go back)

--- src/Pages/MyOrder/index.tsx
+++ src/Pages/MyOrder/index.tsx
@@ -1,24 +1,35 @@
-import { useContext } from "react";
-import { ShoppingCartContext } from "../../Context";
-import OrderCard from "../../Components/OrderCard";
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context';
+import OrderCard from '../../Components/OrderCard';
+import { Link } from 'react-router-dom';
+import { ChevronLeft } from 'lucide-react';
 
 function MyOrder() {
   const context = useContext(ShoppingCartContext);
 
-  return <>
-    MyOrder
-    <div className='flex flex-col w-80'>
-      {context.order?.slice(-1)[0].products.map((product) => (
-        <OrderCard
-          key={product.id}
-          id={product.id}
-          name={product.name}
-          imageURL={product.imageURL}
-          price={product.price}
-        />
-      ))}
-    </div>
-  </>;
+  return (
+    <>
+      <div className='flex w-80 items-center relative justify-center mb-6'>
+        <Link to={'/my-orders'} className='absolute left-0'>
+          <ChevronLeft />
+        </Link>
+        <h1>My Order</h1>
+      </div>
+      <div className='flex flex-col w-80'>
+        {context.order
+          ?.slice(-1)[0]
+          .products.map((product) => (
+            <OrderCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              imageURL={product.imageURL}
+              price={product.price}
+            />
+          ))}
+      </div>
+    </>
+  );
 }
 
 export default MyOrder;
